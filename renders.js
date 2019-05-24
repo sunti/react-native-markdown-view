7a881f2d95d1c96b6da397ad509623748eaf929d
Sunti Suramitmaitree <sunti_@hotmail.com>
2019-05-24 13:52:32 +0700

    improve table
    improve table ability to support cell alignment syntax

--- renders.js
+++ renders.js
@@ -36,7 +36,7 @@ function renderImage(node: ImageNode, output: OutputFunction, state: RenderState
   )
 }
 
-function renderTableCell(cell, row, column, rowCount, columnCount, output, state, styles) {
+function renderTableCell(cell, row, column, rowCount, columnCount, output, state, styles, align) {
   const cellStyle = [styles.tableCell]
   const contentStyle = [styles.tableCellContent]
 
@@ -69,6 +69,8 @@ function renderTableCell(cell, row, column, rowCount, columnCount, output, state
     contentStyle.push(styles.tableCellContentLastColumn)
   }
 
+  contentStyle.push({ textAlign: align || 'center' })
+  
   return <Cell rowId={row} id={column} key={column} style={cellStyle}>
     <Text style={contentStyle}>
       {output(cell, state)}
@@ -172,7 +174,7 @@ export default Object.freeze({
         {node.header.map((cell, column) => renderTableCell(cell, 1, column + 1, node.cells.length + 1, node.header.length, output, state, styles))}
       </Row>].concat(node.cells.map((cells, row) => (
         <Row id={row + 2} key={row + 2}>
-          {cells.map((cell, column) => renderTableCell(cell, row + 2, column + 1, node.cells.length + 1, cells.length, output, state, styles))}
+          {cells.map((cell, column) => renderTableCell(cell, row + 2, column + 1, node.cells.length + 1, cells.length, output, state, styles, node.align[column]))}
         </Row>
       )))}
     </Grid>
